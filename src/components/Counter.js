1a8c508e215a64bbab9b32454fe6cc802eb7294f
bidanee <dleksql119@gmail.com>
2023-09-11 17:29:37 +0900

    use Redux CounterApp

---
+++ src/components/Counter.js
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+import { styled } from "styled-components";
+
+function Counter({ number, diff, onIncrease, onDecrease, onSetDiff }) {
+  const [inputValue, setInputValue] = useState(diff);
+  const onChange = (e) => {
+    const value = parseInt(e.target.value, 10);
+    setInputValue(value);
+    onSetDiff(value);
+  };
+  const isInputValid = !isNaN(inputValue) && inputValue > 0;
+  return (
+    <Container>
+      <Wrap>
+        <Title>Redux 공부 : 카운터</Title>
+
+        <h2>count : {number}</h2>
+        <ItemBox>
+          <Btn onClick={onDecrease} disabled={!isInputValid}>
+            -
+          </Btn>
+          <Input type="number" value={diff} onChange={onChange} />
+          <Btn onClick={onIncrease} disabled={!isInputValid}>
+            +
+          </Btn>
+        </ItemBox>
+      </Wrap>
+    </Container>
+  );
+}
+
+export default Counter;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 300px;
+  border: 3px solid black;
+  border-radius: 16px;
+`;
+
+const Title = styled.h1`
+  font-size: 32px;
+  font-weight: bold;
+`;
+
+const ItemBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Btn = styled.button`
+  width: 36px;
+  height: 36px;
+  font-size: 20px;
+  font-weight: bolder;
+  background-color: skyblue;
+  color: white;
+  border: 2px solid #6aa6bf;
+  border-radius: 4px;
+  margin-inline: 2px;
+`;
+
+const Input = styled.input`
+  &::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  &::-webkit-outer-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  width: 100px;
+  height: 30px;
+  border: 2px solid #6aa6bf;
+  font-size: 20px;
+  text-align: center;
+`;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-/* test */
-// import "./exercise";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
+import counter from "./modules/counter";
+
+const store = createStore(counter);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
